bd87fb823205ca4e2482fe9863cb5806ca6791fe
hmc <hmc@zhiruyi.com>
2021-12-29 14:38:37 +0800

    尝试用useFetch 获取信息

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
-<script lang="ts" setup>
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from './components/HelloWorld.vue';
-  // import { Pagination, Scrollbar, A11y } from 'swiper';
-  // import 'swiper/swiper-bundle.css';
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <div class="test text-3xl">法发顺丰</div>
+  <div>{{ data }}</div>
 </template>
+<script lang="ts" setup>
+  import { watch } from 'vue';
+  import { useFetch } from '@vueuse/core';
+  import HelloWorld from './components/HelloWorld.vue';
+
+  const { data } = useFetch(
+    'http://127.0.0.1:6806/api/query/sql',
+    {
+      mode: 'cors',
+      body: JSON.stringify({
+        stmt: "SELECT * FROM blocks WHERE path regexp '^/20211116085932-8rwiuh5' and path regexp '/{1}' and type = 'd'",
+      }),
+    },
+    {},
+  )
+    .post()
+    .json();
+  watch(data, (value) => {
+    console.log('value', value);
+  });
+  console.log(data);
+</script>
 
 <style lang="scss" scoped>
   .test {
